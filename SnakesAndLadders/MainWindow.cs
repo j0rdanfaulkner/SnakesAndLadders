5be960adabf2d610cb17a48fa54dcb1c70659f43
Jordan Faulkner <jfaulkner2j@icloud.com>
2023-02-10 17:15:39 +0000

    added method for checking if the current player has landed on a snake or a ladder and returning the value of their new position

--- SnakesAndLadders/MainWindow.cs
+++ SnakesAndLadders/MainWindow.cs
@@ -22,19 +22,92 @@ namespace SnakesAndLadders
         {
             positionP1 = 0;
             positionP2 = 0;
+            lblPlayer1PositionNumber.Text = positionP1.ToString();
+            lblPlayer2PositionNumber.Text = positionP2.ToString();
+        }
+        private void UpdateLabels()
+        {
+            lblPlayer1PositionNumber.Text = positionP1.ToString();
+            lblPlayer2PositionNumber.Text = positionP2.ToString();
+        }
+        private void CheckForWinner()
+        {
+            if (positionP1 == 100)
+            {
+                MessageBox.Show("Player 1 Wins!", "We Have a Winner", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            }
+            else if (positionP2 == 100)
+            {
+                MessageBox.Show("Player 2 Wins!", "We Have a Winner", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            }
+        }
+        private int CheckCurrentPosition(int playerPosition)
+        {
+            int currentPosition = playerPosition;
+            // all the spaces that have snakes
+            if (playerPosition == 17)
+            {
+                currentPosition = playerPosition - 10;
+            }
+            else if (playerPosition == 54 || playerPosition == 93 || playerPosition == 95)
+            {
+                currentPosition = playerPosition - 20;
+            }
+            else if (playerPosition == 62)
+            {
+                currentPosition = playerPosition - 43;
+            }
+            else if (playerPosition == 64)
+            {
+                currentPosition = playerPosition - 4;
+            }
+            else if (playerPosition == 87)
+            {
+                currentPosition = playerPosition - 63;
+            }
+            // all the spaces that have ladders
+            else if (playerPosition == 1)
+            {
+                currentPosition = playerPosition + 37;
+            }
+            else if (playerPosition == 4)
+            {
+                currentPosition = playerPosition + 10;
+            }
+            else if (playerPosition == 9)
+            {
+                currentPosition = playerPosition + 22;
+            }
+            else if (playerPosition == 21)
+            {
+                currentPosition = playerPosition + 42;
+            }
+            else if (playerPosition == 28)
+            {
+                currentPosition = playerPosition + 56;
+            }
+            else if (playerPosition == 51)
+            {
+                currentPosition = playerPosition + 16;
+            }
+            else if (playerPosition == 71 || playerPosition == 80)
+            {
+                currentPosition = playerPosition + 20;
+            }
+            return currentPosition;
         }
         private void EndCurrentTurn(int playerNumber)
         {
             if (playerNumber == 1)
             {
                 currentPlayer = 2;
-                // update labels
             }
             else if (playerNumber == 2)
             {
                 currentPlayer = 1;
-                // update labels
             }
+            UpdateLabels();
+            CheckForWinner();
         }
         private int RollDice(int playerNumber)
         {
@@ -49,6 +122,7 @@ namespace SnakesAndLadders
             {
                 RollDice(currentPlayer);
                 positionP1 = positionP1 + rolledNumber;
+                positionP1 = CheckCurrentPosition(positionP1);
             }
             else if (currentPlayer == 2) 
             {
