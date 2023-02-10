58f54fe822877f317285e5ee0376ed78dca08b7b
Jordan Faulkner <jfaulkner2j@icloud.com>
2023-02-10 16:42:21 +0000

    added methods for starting and resetting the game as well as for ending the current player's turn after they roll the dice

--- SnakesAndLadders/MainWindow.cs
+++ SnakesAndLadders/MainWindow.cs
@@ -9,25 +9,53 @@ namespace SnakesAndLadders
         public MainWindow()
         {
             InitializeComponent();
+            StartGame();
         }
-        private int RollDice()
+        private void StartGame()
+        {
+            ResetGame();
+            Random startingPlayer = new Random();
+            currentPlayer = startingPlayer.Next(1, 3);
+            MessageBox.Show("Player " + currentPlayer + " shall go first!", "Starting Player", MessageBoxButtons.OK, MessageBoxIcon.Information);
+        }
+        private void ResetGame()
+        {
+            positionP1 = 0;
+            positionP2 = 0;
+        }
+        private void EndCurrentTurn(int currentPlayer)
+        {
+            if (currentPlayer == 1)
+            {
+                currentPlayer = 2;
+                // update labels
+            }
+            else if (currentPlayer == 2)
+            {
+                currentPlayer = 1;
+                // update labels
+            }
+        }
+        private int RollDice(int currentPlayer)
         {
             Random roll = new Random();
             rolledNumber = roll.Next(1, 7);
+            MessageBox.Show("Player " + currentPlayer + " rolled a " + rolledNumber + "!", "Dice Rolled", MessageBoxButtons.OK, MessageBoxIcon.Information);
             return rolledNumber;
         }
         private void btnRollDice_Click(object sender, EventArgs e)
         {
             if (currentPlayer == 1)
             {
-                RollDice();
+                RollDice(currentPlayer);
                 positionP1 = positionP1 + rolledNumber;
             }
             else if (currentPlayer == 2) 
             {
-                RollDice();
+                RollDice(currentPlayer);
                 positionP2 = positionP2 + rolledNumber;
             }
+            EndCurrentTurn(currentPlayer);
         }
     }
 }
